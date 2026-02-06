6760fe9723b20936176339a353a7aa2731eac8b0
Giacomo <giacomo.cesaroni@edu.unifi.it>
2026-02-06 19:51:17 +0100

    Implementato removeActivity

--- ActivityTracker.cpp
+++ ActivityTracker.cpp
@@ -5,3 +5,13 @@ ActivityTracker::ActivityTracker() {}
 void ActivityTracker::addActivity(QDate date, Activity activity) {
     activities[date].push_back(activity);
 }
+
+void ActivityTracker::removeActivity(QDate date, const Activity &activity) {
+    auto it = activities.find(date);
+    if (it != activities.end()) {
+        it->second.remove(activity);
+        if (it->second.empty()) {
+            activities.erase(it);
+        }
+    }
+}
