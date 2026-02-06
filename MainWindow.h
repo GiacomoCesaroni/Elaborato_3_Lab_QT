99dc436347341da58241f2d493505321310c5ecf
Giacomo <giacomo.cesaroni@edu.unifi.it>
2026-02-06 21:00:05 +0100

    Aggiunte dichiarazioni forward di classi in MainWindow.h

---
+++ MainWindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QDialog>
+#include <QMainWindow>
+
+class QTextBrowser;
+class QPushButton;
+class QVBoxLayout;
+class QSpinBox;
+class QLineEdit;
+class QTextEdit;
+class QGroupBox;
+class QGridLayout;
+class QHBoxLayout;
+class QLabel;
+class QTimeEdit;
+
+class ActivityTracker;
+
+
+class MainWindow {
+
+};
+
+
+
+#endif //MAINWINDOW_H
